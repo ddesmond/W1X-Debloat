93e8e55d0998700c7a6d0986c04f5903afbf5b7a
Aleks <crni.aki@gmail.com>
2025-04-12 09:02:44 +0200

    Update W1X Debloat.ps1

--- W10-11/W1X Debloat.ps1
+++ W10-11/W1X Debloat.ps1
@@ -928,21 +928,7 @@ powercfg /Change -disk-timeout-dc 0
 powercfg /Change -disk-timeout-ac 0
 Write-Host "Sleep Settings: Hard Drive (Battery: Never | AC: Never)" -ForegroundColor Green
 
-powercfg /Change -hibernate-timeout-ac 0
-powercfg /Change -hibernate-timeout-dc 0
-powercfg -h off
-Write-Host "Sleep Settings: Hibernate [DISABLED]" -ForegroundColor Green
-
-powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-Write-Host "Sleep Settings: 'Closing Lid' action to turn off screen" [CHANGED] -ForegroundColor Green
-
-if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings") -ne $true) {New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" -force | Out-Null}
-New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings' -Name 'ShowSleepOption' -Value "0" -PropertyType DWord -Force | Out-Null
-New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings' -Name 'ShowHibernateOption' -Value "0" -PropertyType DWord -Force | Out-Null
-Write-Host "Sleep Settings: Sleep/Hibernate from Start Menu [DISABLED]" -ForegroundColor Green
 #endregion
-
 
 <#############################################################################################################################>
 #region 7.0 - Privacy
